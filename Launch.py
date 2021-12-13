e21b001fa56fdd9e4e9a77e4088019caea5455b4
Jovan <jovanpop92@gmail.com>
2021-12-13 20:51:33 +0100

    Create python projekat. Close #3

---
+++ Launch.py
@@ -0,0 +1,22 @@
+"""
+MIT license
+
+Authors:
+Novica Nikolic - R2 39/2020
+Jovan Popovic - R2 40/2020
+Dejan Jovanovic - R2 45/2020
+
+Domain-Specific Languages DSL, FTN 2021
+"""
+
+
+def init():
+    """
+    Method for running the project.
+    :return:
+    """
+    print("Chess is running successfully ...")
+
+
+if __name__ == "__main__":
+    init()
