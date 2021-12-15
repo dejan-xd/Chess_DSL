3bbc4bf36024897efbe4b8df792e0f4bde4fc754
dejan-xd <dejan.jovanovic94@gmail.com>
2021-12-15 21:43:26 +0100

    Create main menu GUI. Close #7

--- Launch.py
+++ Launch.py
@@ -9,13 +9,26 @@ Dejan Jovanovic - R2 45/2020
 Domain-Specific Languages DSL, FTN 2021
 """
 
+import GUI
+import Json
+import pygame as p
+
 
 def init():
     """
     Method for running the project.
     :return:
     """
-    print("Chess is running successfully ...")
+    json_data = Json.read_from_json()
+    p.init()  # initializing the constructor
+    p.display.set_caption(json_data['NAME'])
+    icon = p.image.load(json_data['IMAGE'])
+    p.display.set_icon(icon)
+    screen = p.display.set_mode((json_data['BOARD_WIDTH'], json_data['BOARD_HEIGHT']))
+    screen.fill((json_data['BACKGROUND_COLOR']['RED'], json_data['BACKGROUND_COLOR']['GREEN'], json_data['BACKGROUND_COLOR']['BLUE']))
+    p.display.flip()
+
+    GUI.main_menu(screen)
 
 
 if __name__ == "__main__":
